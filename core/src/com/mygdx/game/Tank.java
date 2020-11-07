76c60efaecedba55b0512288f2204d2f8941e9ea
senyabird <senyabird@yandex.ru>
2020-11-07 10:53:08 +0300

    Initial commit.

--- core/src/com/mygdx/game/Tank.java
+++ core/src/com/mygdx/game/Tank.java
@@ -17,6 +17,22 @@ public class Tank {
     private final Projectile projectile;
     private final float scale;
 
+//    private final float X_REAR_LEFT_CORNER = x - tankHeight * MathUtils.sin(angle);
+//    private final float Y_REAR_LEFT_CORNER = y + tankHeight * MathUtils.cos(angle);
+//    private final float X_REAR_RIGHT_CORNER = x;
+//    private final float Y_REAR_RIGHT_CORNER = y;
+//    private final float X_FRONT_LEFT_CORNER = x + tankLength * MathUtils.cos(angle);
+//    private final float Y_FRONT_LEFT_CORNER = y + tankLength * MathUtils.sin(angle);
+//    private final float X_FRONT_RIGHT_CORNER = x + tankLength * MathUtils.cos(angle) -
+//            tankHeight * MathUtils.sin(angle);
+//    private final float Y_FRONT_RIGHT_CORNER = y + tankLength * MathUtils.sin(angle) +
+//            tankHeight * MathUtils.cos(angle);
+//
+//    private final float[] xCornerCoordinates = {X_REAR_LEFT_CORNER, X_REAR_RIGHT_CORNER,
+//            X_FRONT_LEFT_CORNER, X_FRONT_RIGHT_CORNER};
+//    private final float[] yCornerCoordinates = {Y_REAR_LEFT_CORNER, Y_REAR_RIGHT_CORNER,
+//            Y_FRONT_LEFT_CORNER, Y_FRONT_RIGHT_CORNER};
+
     public Tank() {
         this.texture = new Texture("tank.png");
         this.textureWeapon = new Texture("weapon.png");
@@ -24,10 +40,13 @@ public class Tank {
         this.x = 100.0f;
         this.y = 100.0f;
         this.speed = 240.0f;
-        this.scale = 3.0f;
+        this.scale = 2.0f;
     }
 
     public void update(float dt) {
+        // 1. Не дайте танку уехать за пределы экрана
+        // Если какой-нибудь из углов танка упёрся в какую-нибудь стенку - остановить танк.
+        // Простейшая реализация (без физики).
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             angle -= 90.0f * dt;
         }
@@ -55,7 +74,8 @@ public class Tank {
             angleWeapon += 90.0f * dt;
         }
         if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && !projectile.isActive()) {
-            projectile.shoot(x + 16 * scale * MathUtils.cosDeg(angle), y + 16* scale * MathUtils.sinDeg(angle), angle + angleWeapon);
+            projectile.shoot(x + 16 * scale * MathUtils.cosDeg(angle + angleWeapon),
+                    y + 16 * scale * MathUtils.sinDeg(angle + angleWeapon), angle + angleWeapon);
         }
         if (projectile.isActive()) {
             projectile.update(dt);
@@ -63,8 +83,10 @@ public class Tank {
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(texture, x - 20, y - 20, 20, 20, 40, 40, scale, scale, angle, 0, 0, 40, 40, false, false);
-        batch.draw(textureWeapon, x - 20, y - 20, 20, 20, 40, 40, scale, scale, angle + angleWeapon, 0, 0, 40, 40, false, false);
+        batch.draw(texture, x - 20, y - 20, 20, 20, 40, 40, scale, scale, angle,
+                0, 0, 40, 40, false, false);
+        batch.draw(textureWeapon, x - 20, y - 20, 20, 20, 40, 40, scale, scale, angle + angleWeapon,
+                0, 0, 40, 40, false, false);
         if (projectile.isActive()) {
             projectile.render(batch);
         }
